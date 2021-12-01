0d9161dfe5ae3bbc699e1e367f703d3aa2cda3fc
yamunanga <77024699+yamunanga@users.noreply.github.com>
2021-12-01 23:31:41 +0530

    Delete SMS new/UI directory

--- SMS new/UI/src/app/models/subject.model.ts
+++
@@ -1,9 +0,0 @@
-//this is for get all subject list
-export class SUBJECTS {
-    _id!: string;
-    teacherId!: number;
-    name!: String;
-    subjectId!:number;
-    createdAt!: string;
-    updatedAt!: String;
-}
